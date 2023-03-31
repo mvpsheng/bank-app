7710b03faf62a2835e3e064ef9323fc681357789
mvpsheng <2711136701@qq.com>
2023-03-31 09:57:59 +0800

    fetchdata

--- app.js
+++ app.js
@@ -1,3 +1,91 @@
+let account = null;
+
+function updateElement(id, textOrNode) {
+  const element = document.getElementById(id);
+  element.textContent = ''; // Removes all children
+  element.append(textOrNode);
+}
+
+function createTransactionRow(transaction) {
+  const template = document.getElementById('transaction');
+  const transactionRow = template.content.cloneNode(true);
+  const tr = transactionRow.querySelector('tr');
+  tr.children[0].textContent = transaction.date;
+  tr.children[1].textContent = transaction.object;
+  tr.children[2].textContent = transaction.amount.toFixed(2);
+  return transactionRow;
+}
+
+function updateDashboard() {
+  if (!account) {
+    return navigate('/login');
+  }
+
+  updateElement('description', account.description);
+  updateElement('balance', account.balance.toFixed(2));
+  updateElement('currency', account.currency);
+
+  const transactionsRows = document.createDocumentFragment();
+  for (const transaction of account.transactions) {
+    const transactionRow = createTransactionRow(transaction);
+    transactionsRows.appendChild(transactionRow);
+  }
+  updateElement('transactions', transactionsRows);
+}
+
+async function login() {
+  const loginForm = document.getElementById('loginForm')
+  const user = loginForm.user.value;
+  const data = await getAccount(user);
+
+  if (data.error) {
+    return updateElement('loginError', data.error);
+  }
+
+  account = data;
+  navigate('/dashboard');
+}
+
+async function getAccount(user) {
+  try {
+    const response = await fetch('//localhost:5000/api/accounts/' + encodeURIComponent(user));
+    return await response.json();
+  } catch (error) {
+    return { error: error.message || 'Unknown error' };
+  }
+}
+
+async function register() {
+  const registerForm = document.getElementById('registerForm');
+  const formData = new FormData(registerForm);
+  const data = Object.fromEntries(formData);
+  const jsonData = JSON.stringify(data);
+  const result = await createAccount(jsonData);
+
+  if (result.error) {
+    if (data.error) {
+      return updateElement('registerError', result.error);
+    }
+  }
+
+  console.log('Account created!', result);
+  account = result;
+  navigate('/dashboard');
+}
+
+async function createAccount(account) {
+  try {
+    const response = await fetch('//localhost:5000/api/accounts', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: account
+    });
+    return await response.json();
+  } catch (error) {
+    return { error: error.message || 'Unknown error' };
+  }
+}
+
 function navigate(path) {
     // console.log(path);
     window.history.pushState({}, path, path);
@@ -6,7 +94,7 @@ function navigate(path) {
 
 const routes = {
     '/login': { templateId: 'login' },
-    '/dashboard': { templateId: 'dashboard' },
+    '/dashboard': { templateId: 'dashboard', init: updateDashboard },
     '/user': {templateId: 'user'},
 };
 
@@ -16,21 +104,26 @@ const titles = {
     'user': {titleId: 'User Info'},
 };
 
-function updateRoute() {
+  function updateRoute() {
     const path = window.location.pathname;
-  const route = routes[path];
-  if (!route) {
-    return navigate('/login');
-  }
-  if(route) {
-    document.title = titles[route.templateId].titleId 
-    console.log(route.templateId + ' is displayed');
-  }
-  const template = document.getElementById(route.templateId);
-  const view = template.content.cloneNode(true);
-  const app = document.getElementById('app');
-  app.innerHTML = '';
-  app.appendChild(view);
+    const route = routes[path];
+    if (!route) {
+      return navigate('/login');
+    }
+    if(route) {
+      document.title = titles[route.templateId].titleId 
+      console.log(route.templateId + ' is displayed');
+    }
+    const template = document.getElementById(route.templateId);
+    const view = template.content.cloneNode(true);
+    const app = document.getElementById('app');
+    app.innerHTML = '';
+    app.appendChild(view);
+    
+    if (typeof route.init === 'function') {
+      route.init();
+    }
+
   }
 
   function onLinkClick(event) {
